f76f3f46e2a27c8dcbeb3b28fa5843f045ec8908
jahidhowlader <jahidhowlader.info@gmail.com>
2025-04-04 19:22:44 +0600

    implement transaction where create user and student

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,18 +1,20 @@
+import mongoose from "mongoose";
 import config from "../../config";
-import AppError from "../../errors/AppError";
 import AcademicSemesterModel from "../academic-semester/academicSemester.model";
 import { TStudent } from "../student/student.interface";
 import { StudentModel } from "../student/student.model";
 import { TUser } from "./user.interface";
 import { UserModel } from "./user.model";
 import { generateStudentId } from "./user.utils";
-// import { StudentModel } from "../student/student.model";
+import AppError from "../../errors/AppError";
+import status from "http-status";
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
 
-    if (!payload) {
-        throw new AppError(400, 'Student data missing')
-    }
+    const userData: Partial<TUser> = {};
+
+    userData.password = password || (config.DEFAULT_PASSWORD as string);
+    userData.role = 'student';
 
     // find academic semester info
     const admissionSemester = await AcademicSemesterModel.findById(
@@ -20,45 +22,44 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     );
 
     if (!admissionSemester) {
-        throw new AppError(400, 'This semester not available')
+        throw new AppError(404, "Admission semester not found");
     }
 
-    const userData: Partial<TUser> = {
-        id: await generateStudentId(admissionSemester),
-        password: password ?? config.DEFAULT_PASSWORD,
-        role: 'student'
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        //set  generated id
+        userData.id = await generateStudentId(admissionSemester);
+
+        // create a user (transaction-1)
+        const newUser = await UserModel.create([userData], { session }); // array
+
+        //create a student
+        if (!newUser.length) {
+            throw new AppError(status.BAD_REQUEST, 'Failed to create user');
+        }
+        // set id , _id as user
+        payload.id = newUser[0].id;
+        payload.user = newUser[0]._id; //reference _id
+
+        // create a student (transaction-2)
+        const newStudent = await StudentModel.create([payload], { session });
+        if (!newStudent.length) {
+            throw new AppError(status.BAD_REQUEST, 'Failed to create student');
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+        return newStudent;
     }
-
-    const newUser = await UserModel.create(userData)
-
-    if (newUser._id) {
-
-        payload.id = newUser.id
-        payload.user = newUser._id
-
-        const newStudent = await StudentModel.create(payload)
-        return newStudent
+    catch (err) {
+        await session.abortTransaction();
+        await session.endSession();
+        if (err instanceof Error) {
+            throw new AppError(status.BAD_REQUEST, err.message || 'Failed to create student');
+        }
     }
-
-
-
-    // const result = await StudentModel.create(payload)
-    // return result
-
-    // For Creating Static Methods
-    // if (await StudentModel.isUserExist(payload.id)) {
-    //     throw new Error('Student Id is already exist')
-    // }
-    // const result = await StudentModel.create(payload)
-    // return result
-
-    // For Creating Instance Methods 
-    // const student = new StudentModel(payload)
-    // if (await student.isUserExist(payload.id)) {
-    //     throw new Error('Student Id is already exist')
-    // }
-    // const result = await student.save()
-    // return result;
 };
 
 export const UserServices = {
