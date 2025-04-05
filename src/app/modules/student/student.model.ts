9e54f272fe1abdf70825b0d3e935db53cc17a159
jahidhowlader <jahidhowlader.info@gmail.com>
2025-04-06 01:17:00 +0600

    add query builder like search, filter, sort, paginate, fields

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -180,7 +180,7 @@ const studentSchema = new Schema<TStudent, TStudentMethods>({
 
 // virtual
 studentSchema.virtual('fullName').get(function () {
-    return this.name.firstName + this.name.middleName + this.name.lastName;
+    return this?.name?.firstName + this?.name?.middleName + this?.name?.lastName;
 });
 
 // Query Middleware
